10c59bb025e71c30f8acf2786bb210b25135196e
Nayana Holanda de Abreu <holanda.nayana@gmail.com>
2021-02-22 08:03:40 +0100

    first commit

---
+++ FlaskExercise/__init__.py
@@ -0,0 +1,25 @@
+import FlaskExercise.views
+import logging
+from logging.config import dictConfig
+from flask import Flask
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['wsgi']
+    }
+})
+
+app = Flask(__name__)
+wsgi_app = app.wsgi_app
+# TODO: Set the app's logger level to "warning"
+#   and any other necessary changes
